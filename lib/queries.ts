f8850e9586052cd664450803d55a03cb17930121
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-04 20:41:07 +0200

    New: Enhance through adding `createNotificationActivity` action

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,8 +1,8 @@
 "use server"
 
-// DONE REVIEWING: GITHUB ACTION 1️⃣
+// DONE REVIEWING: GITHUB ACTION 2️⃣
 
-import {currentUser} from "@clerk/nextjs/server"
+import {clerkClient, currentUser} from "@clerk/nextjs/server"
 import {User} from "@prisma/client"
 import {redirect} from "next/navigation"
 import {db} from "./db"
@@ -28,6 +28,64 @@ export const getUser = async function getUser() {
   })
 
   return data
+}
+
+export const createNotificationActivity = async function createNotificationActivity({
+  agencyId,
+  subAccountId,
+  description
+}: {
+  agencyId?: string
+  subAccountId?: string
+  description: string
+}) {
+  const user = await currentUser()
+  let data
+
+  if (!user) {
+    const response = await db.user.findFirst({
+      where: {
+        agency: {
+          sub_accounts: {
+            some: {
+              id: subAccountId
+            }
+          }
+        }
+      }
+    })
+
+    if (response) data = response
+  } else data = await db.user.findUnique({where: {email: user.emailAddresses[0].emailAddress}})
+
+  if (!data) throw new Error("Error: could not find a user.")
+
+  let notificationActivityAgencyId = agencyId
+  if (!notificationActivityAgencyId) {
+    if (!subAccountId)
+      throw new Error("Error: you need to provide at least an agency or sub-account ID.")
+
+    const response = await db.subAccount.findUnique({where: {id: subAccountId}})
+    if (response) notificationActivityAgencyId = response.agency_id
+  }
+
+  if (subAccountId)
+    await db.notification.create({
+      data: {
+        notification: [data.name, description].join(" | "),
+        user: {connect: {id: data.id}},
+        agency: {connect: {id: notificationActivityAgencyId}},
+        sub_account: {connect: {id: subAccountId}}
+      }
+    })
+  else
+    await db.notification.create({
+      data: {
+        notification: [data.name, description].join(" | "),
+        user: {connect: {id: data.id}},
+        agency: {connect: {id: data.agency_id}}
+      }
+    })
 }
 
 export const createUserTeam = async function createUserTeam(user: User, agencyId: string) {
@@ -63,6 +121,26 @@ export const verifyAcceptInvitation = async function verifyAcceptInvitation() {
       invitation.agency_id
     )
 
-    return data
+    await createNotificationActivity({
+      agencyId: invitation?.agency_id,
+      subAccountId: undefined,
+      description: "has successfully joined."
+    })
+
+    if (data) {
+      await clerkClient.users.updateUserMetadata(user.id, {
+        privateMetadata: {
+          role: data.role || "SUB_ACCOUNT_USER"
+        }
+      })
+
+      await db.invitation.delete({where: {email: data.email}})
+      return data.agency_id
+    }
+
+    return null
   }
+
+  const agency = await db.user.findUnique({where: {email: user.emailAddresses[0].emailAddress}})
+  return agency ? agency.agency_id : null
 }
