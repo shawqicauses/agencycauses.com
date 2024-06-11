6cff98ce006fc3e86f0cf136368b6336062b5cd7
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-11 23:41:37 +0200

    New: Enhance through adding `logo`, `isAgencyWithLabel`, `subAccounts`, and `options`

--- components/side-navigation.tsx
+++ components/side-navigation.tsx
@@ -1,4 +1,4 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
 import {getUser} from "../lib/queries"
 
 type SideNavigationProps = {
@@ -15,6 +15,28 @@ const SideNavigation = async function SideNavigation({id, type}: SideNavigationP
     type === "AGENCY"
       ? user.agency
       : user.agency.sub_accounts.find((subAccount) => subAccount.id === id)
+
+  if (!data) return null
+
+  let logo = user.agency.logo_url
+  const isAgencyWithLabel = user.agency.with_label
+  if (!isAgencyWithLabel)
+    if (type === "SUB_ACCOUNT")
+      logo =
+        user.agency.sub_accounts.find((subAccount) => subAccount.id === id)?.logo_url ||
+        user.agency.logo_url
+
+  const subAccounts = user.agency.sub_accounts.filter((subAccount) =>
+    user.permissions.find(
+      (permission) => permission.sub_account_id === subAccount.id && permission.access
+    )
+  )
+
+  const options =
+    type === "AGENCY"
+      ? user.agency.sidebar_options || []
+      : user.agency.sub_accounts.find((subAccount) => subAccount.id === id)?.sidebar_options || []
+
   return <div>Side Navigation</div>
 }
 
